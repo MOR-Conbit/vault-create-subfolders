17ee0a0020a4cd2083ca02cfec6bfdefbb7d66cd
MOR-Conbit <mor@conbit.nl>
2026-07-13 12:07:57 +0200

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("API Onboarding - Event Handler")]
-[assembly: AssemblyDescription("API Introduction Sample 03 - Event Handler")]
+[assembly: AssemblyTitle("CONBIT Create Project Folder")]
+[assembly: AssemblyDescription("Autodesk Vault Event Handler for automatic project folder creation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk")]
-[assembly: AssemblyProduct("Autodesk Vault Professional - API Sample")]
-[assembly: AssemblyCopyright("Copyright © Autodesk 2024")]
+[assembly: AssemblyCompany("CONBIT")]
+
+//Project name
+[assembly: AssemblyProduct("CONBIT Create Project Folder")]
+
+//Copyright
+[assembly: AssemblyCopyright("Copyright © 2026 Max van Orsouw")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1e7fb205-0d13-46d9-9d6c-6409b31782c5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("30.0.91.0")]
-[assembly: AssemblyFileVersion("30.0.91.0")]
